1161e98976dfd9c207827fa1292631ba267ae352
amm.soft.dev@gmail.com <amm.soft.dev@gmail.com>
2018-12-12 09:44:44 -0200

    Meu primeiro commit do curso

---
+++ Exercicio_01/hello.js
@@ -0,0 +1,10 @@
+
+
+console.log("Hello, World Node.js!")
+
+var nome = "Samuel";
+var sobrenome = "Ferreira Duarte";
+var idade = 33;
+
+
+console.log("O nome do cara é:", nome, sobrenome, " e sua idade é:", idade);
