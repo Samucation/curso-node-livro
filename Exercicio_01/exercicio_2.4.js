c2886e14c1a2b32ffe51e1d06db23082fa9f4052
amm.soft.dev@gmail.com <amm.soft.dev@gmail.com>
2018-12-12 10:15:11 -0200

    Brincando um pouco com node exercicio 2.4 com mais algumas coisas

--- Exercicio_01/exercicio_2.4.js
+++ Exercicio_01/exercicio_2.4.js
@@ -6,15 +6,28 @@
 function metodoComAtributoVazio() {
     var cabelosBons = 33000;
     var cabelosBrancos = 5000;
-
     var totalCabelos = cabelosBons + cabelosBrancos;
-
     var nome; //Atributo definido sem valor
 
     console.log("A quantidade de cabelos brancos do indivíduo é %d e a quantidade de cabelos bons %d", cabelosBrancos, cabelosBons);
     console.log("O total de cabelos é %d", totalCabelos);
+    console.log("\n\n## Tipos de atributos da função ##");
+    console.log("O tipo do atributo nome é %s \nO tipo do atributo cabelosBons é %s\nO tipo do atributo cabelosBrancos é %s", typeof(nome), typeof(cabelosBons), typeof(cabelosBrancos));
+    console.log("\nVamos imprimir esse atributo nome vazio e ver o que retorna %s \n", nome);
 
-    console.log("Vamos imprimir esse atributo nome vazio e ver o que retorna %d", nome);
+    return nome;
 }
 
-metodoComAtributoVazio();
+var recebeAtributoStringVazio = metodoComAtributoVazio();
+console.log("Iniciando testes de retorno da função");
+
+//Testando os testes de retorno da função
+if(recebeAtributoStringVazio == undefined) {
+    console.log("1 - O atributo recebeAtributoStringVazio não tem valor definido");
+} else if (recebeAtributoStringVazio == "undefined") {
+    console.log("2 - O atributo recebeAtributoStringVazio não tem valor definido");
+} else {
+    console.log("Nenhum dos testes foi válido");
+}
+
+console.log("\nFim do nosso sisteminha");
