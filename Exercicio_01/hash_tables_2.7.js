5d44d43bd59200045446393508a59c129b113eec
amm.soft.dev@gmail.com <amm.soft.dev@gmail.com>
2018-12-12 10:47:49 -0200

    Iniciando projeto hash tables exercicio 2.7

---
+++ Exercicio_01/hash_tables_2.7.js
@@ -0,0 +1,13 @@
+
+/** Validando estrutura de Hash tables (Mapas) em JS Node */
+
+function testandoMapa() {
+    var mapa =[]
+    mapa["nome"] = "Samuel"
+    mapa["sobrenome"] = "Ferreira Duarte"
+
+    console.log(mapa)
+    console.log("Nome completo:", mapa["nome"], mapa["sobrenome"])
+}
+
+testandoMapa();
