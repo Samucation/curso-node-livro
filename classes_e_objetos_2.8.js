a259f279ee827c86a6a45dff11321f02d3707be6
amm.soft.dev@gmail.com <amm.soft.dev@gmail.com>
2018-12-27 09:41:26 -0200

    Ajustando as nomenclaturas do sistema

--- classes_e_objetos_2.8.js
+++ classes_e_objetos_2.8.js
@@ -4,7 +4,7 @@
 //Declarando um objeto JS direto usando Object, aparentemente a logica é inversa
 //de uma linguagem como java, aqui se declara ja usando!
 function objetoJS() {
-    console.log("\n\n## Usando a declaração de Objeto em JS com a função Object() ##");
+    console.log("\n\n1° - ## Usando a declaração de Objeto em JS com a função Object() ##");
 
     var pessoa = Object();
     pessoa.nome = "Samuel";
@@ -29,7 +29,7 @@ console.log("\n-----------------------------------------------------------------
 
 //Usando a declaração de classe direto em sua nomeação, a caracteristica deste método é que tudo é separado por virgulas.
 function objetoJSNumeroDois() {
-    console.log("\n## Usando a declaração de Objeto em JS usando outra sintaxe separando por virgulas! ##");
+    console.log("\n2° - ## Usando a declaração de Objeto em JS usando outra sintaxe separando por virgulas! ##");
 
     var pessoa = {
         nome : 'Samuel',
@@ -51,7 +51,7 @@ function objetoJSNumeroDois() {
 objetoJSNumeroDois();
 
 console.log("\n--------------------------------------------------------------------------------------------------------");
-console.log("Agora vamos pegar e vamos verificar como fica a sintaxe de objeto mais usada em java script ECMA\n");
+console.log("3° - Agora vamos pegar e vamos verificar como fica a sintaxe de objeto mais usada em java script ECMA\n");
 
 //Criando o objeto isolado com suas dependencias, maneira mais usada fica tudo emcapsulado, logica proxima ao JAVA comum.
 function Pessoa() {
@@ -64,8 +64,8 @@ function Pessoa() {
         return "Olá meu jovem!";
     }
 }
-
-function funcaoQueCamaObjTres() {
+//Chamando  objeto isolado com suas dependencias, ele está emcapsulado dentro de uma função.
+function funcaoQueChamaObjTres() {
     var pessoa =  new Pessoa();
 
     console.log(pessoa);
@@ -73,11 +73,12 @@ function funcaoQueCamaObjTres() {
     console.log(pessoa.sobrenome);
     console.log(pessoa.saudacao);
 }
-funcaoQueCamaObjTres();
+funcaoQueChamaObjTres();
 
 console.log("\n--------------------------------------------------------------------------------------------------------");
-console.log("Novamente veremos mais uma forma de declarar o java script ECMA agora usando uma linguagem próxima a de linguagens já conhecidas como JAVA");
+console.log("4° - Novamente veremos mais uma forma de declarar o java script ECMA agora usando uma linguagem próxima a de linguagens já conhecidas como JAVA!");
 
+//A classe é declarada exatamente como no JAVA, é possível até inserir metodos estaticos que não necessitam serem instanciados.
 class PessoaDois {
     constructor() {
         this.nome = "Samuel";
@@ -92,14 +93,14 @@ class PessoaDois {
         return "Olá eu jovem, eu sou uma saudação estatica!";
     }
 }
-
+//A função quatro chama o objeto PessoaDois e imprime nos consoles seus atributos e métodos, inclusive o método estatico.
 function funcaoQueChamaObjQuatro() {
     var pessoa = new PessoaDois();
     console.log(pessoa);
     console.log(pessoa.nome);
     console.log(pessoa.sobrenome);
     console.log(pessoa.saudacao());
-    console.log(PessoaDois.saudacaoEstatica());
+    console.log(PessoaDois.saudacaoEstatica()); //Saudação estatica não precisou ser instanciado, ele como o nome sugere, é estatico!
 }
 funcaoQueChamaObjQuatro();
 
